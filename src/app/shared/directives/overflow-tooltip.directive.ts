9e4d517bef8fb982a8a8ae3142fea741da769e0c
Ash <shahbazyan.ash7@gmail.com>
2026-06-23 09:22:01 +0400

    added wedding

---
+++ src/app/shared/directives/overflow-tooltip.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, ElementRef, HostListener, Input, OnChanges } from '@angular/core';
+
+// Shows pTooltip only when the element's text is actually truncated (scrollWidth > clientWidth)
+@Directive({
+  selector: '[overflowTooltip]',
+  standalone: true,
+  host: {
+    '[attr.title]': 'null', // prevent native tooltip
+  }
+})
+export class OverflowTooltipDirective implements OnChanges {
+  @Input('overflowTooltip') tooltipText: string = '';
+
+  constructor(private el: ElementRef<HTMLElement>) {}
+
+  ngOnChanges() {
+    this.update();
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.update();
+  }
+
+  private update() {
+    const el = this.el.nativeElement;
+    const isOverflowing = el.scrollWidth > el.clientWidth;
+    el.title = isOverflowing ? this.tooltipText : '';
+  }
+}
